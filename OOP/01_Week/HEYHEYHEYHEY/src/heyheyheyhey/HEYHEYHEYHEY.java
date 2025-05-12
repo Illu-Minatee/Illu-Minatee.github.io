23650b03ac2c2d44995340dfeb914799fcf6dc11
Illu-Minatee <nischal.shresthakasula@dcmail.ca>
2025-05-12 09:50:45 -0400

    backing up everything for this semester.

---
+++ OOP/01_Week/HEYHEYHEYHEY/src/heyheyheyhey/HEYHEYHEYHEY.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package heyheyheyhey;
+
+import java.util.Scanner;
+
+public class HEYHEYHEYHEY {
+// final means it's a constaant 
+        final String SET_TITLE = "\033]0;TITLEEEE\007";
+        
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("");
+        
+        // TODO code application logic here
+        System.out.println("HY");
+        
+        // Scanner - gets the user input 
+        
+        
+        // 
+    }
+    
+}
