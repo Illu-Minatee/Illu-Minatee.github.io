1dad05dcfab6c85f4cf97b0bc60d10b2a2db5033
Illu-Minatee <nischal.shresthakasula@dcmail.ca>
2025-07-17 17:22:32 -0400

    Added basic variables and helper function

--- INFT_1206-03_(WEB)/A_4/Part_1/main.js
+++ INFT_1206-03_(WEB)/A_4/Part_1/main.js
@@ -4,3 +4,13 @@ File: main.js
 Date: 17 July 2025
 JavaScript for generating a silly story with random elements and user customization.
 */
+
+// 1. COMPLETE VARIABLE AND FUNCTION DEFINITIONS
+const customName = document.getElementById('customname');
+const randomize = document.querySelector('.randomize');
+const story = document.querySelector('.story');
+
+function randomValueFromArray(array){
+  const random = Math.floor(Math.random() * array.length);
+  return array[random];
+}
