380a1f992767ec198a28794412485672282ec8ca
Illu-Minatee <nischal.shresthakasula@dcmail.ca>
2025-07-18 15:21:49 -0400

    added js animation part and git push is finally working. Also fixed the height and width issues for the pictures.

--- INFT_1206-03_(WEB)/A_4/Part_3/script.js
+++ INFT_1206-03_(WEB)/A_4/Part_3/script.js
@@ -4,3 +4,23 @@ File: script.js
 Date: 18 July 2025
 Simple animations for resume page.
 */
+
+window.addEventListener('load', () => {
+    document.querySelectorAll('section').forEach(sec => {
+      sec.style.opacity = 0;
+      setTimeout(() => {
+        sec.style.transition = 'opacity 1s ease-in';
+        sec.style.opacity = 1;
+      }, 300);
+    });
+  });
+  
+  // Profile image pulse effect
+  const profilePic = document.querySelector('.profile-pic');
+  profilePic.addEventListener('click', () => {
+    profilePic.style.transition = 'transform 0.5s';
+    profilePic.style.transform = 'scale(1.1)';
+    setTimeout(() => {
+      profilePic.style.transform = 'scale(1)';
+    }, 500);
+  });
